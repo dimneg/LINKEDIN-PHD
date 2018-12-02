b82ecfade9ab819cf51d6ce2c6e5a857c18139e8
dimitris negkas <dimneg@gmail.com>
2018-12-02 20:24:54 +0200

    attributes

--- LinkedIn.php
+++ LinkedIn.php
@@ -43,9 +43,32 @@ class LinkedIn {
         return $tokenArray[0];
     }
     function TxtGetLinkedInLink($tokenArray){
-        #foreach ($tokenArray as $key => $value) {
-            $result = str_replace('www.linkedin.com/in/','',$tokenArray[1].$tokenArray[2]);
-        #}
+        $result ='';      
+        $resultArray=[];
+        $keywords_af =['﻿(LinkedIn)'];
+        $keywords_bf =['﻿﻿Contact'];
+        
+        foreach ($tokenArray as $key => $row) {
+             #if ($tokenArray[$key]==  $keywords_af[0]) {
+             if ($this->stringCompare($keywords_af[0], $tokenArray[$key])> 80)  { 
+                 echo 'found'.$tokenArray[$key].PHP_EOL;
+                 $index = $key - 1;
+              #   while ( strpos($tokenArray[$index],  $keywords_bf[0]) === 'false' )  {   
+                  while ( $this->stringCompare($keywords_bf[0], $tokenArray[$index])< 80 && $this->isEmail($tokenArray[$index]) === FALSE )  {               
+#while ($this->isEmail($tokenArray[$key])===FALSE ||( strpos($tokenArray[$index],  $keywords_bf[0]) ===FALSE ) ) {
+                     
+                      $resultArray []= $tokenArray[$index];
+                      echo 'index='. $index;
+                      $index-- ;
+                 }
+                 #$result = str_replace('www.linkedin.com/in/','',$result );
+                 return implode("", array_reverse($resultArray));
+             }
+             else {
+                # echo $tokenArray[$key].' '.$keywords_af[0].' '.  $this->stringCompare(preg_replace('/\s+/', '', $tokenArray[$key]),  preg_replace('/\s+/', '', $keywords_af[0])).PHP_EOL;
+             }
+            #$result = str_replace('www.linkedin.com/in/','',$tokenArray[1].$tokenArray[2]);
+        }
         return $result;
         
     }
@@ -63,6 +86,21 @@ class LinkedIn {
         
     }
     
+    function getArrayTokensBetween ($tokenArray,$token_bf, $token_af){
+         foreach ($tokenArray as $key => $row) {
+             
+         }
+        
+    }
+    
+    function isEmail($string){
+        if  (strpos($string, '@')!==false && strpos($string, '.')!==false){
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
     
     function readCsv($inputPath,$file){
         $tokenArray=[];
@@ -76,4 +114,13 @@ class LinkedIn {
          }
          return $tokenArray;
     }
+    
+     function stringCompare($string1, $string2) {
+        $percentage = 0;
+        // similar_text(mb_strtolower($string1,'UTF-8'), mb_strtolower($string2,'UTF-8'), $percentage); 
+        similar_text($string1, $string2, $percentage);
+        return $percentage;
+    }
+    
+    
 }
